444755e514de2ba6d33859e31e480c54f1010e5e
Emils <emils@gmail.com>
2025-10-20 08:59:11 +0300

    Added groupName, Number and activity

---
+++ variables.js
@@ -0,0 +1,15 @@
+const groupName = "PT25";
+console.log("PT25")
+
+Number("23");
+console.log("23")
+
+
+if ("isntActive = true") {
+  console.log("isntActive");
+}
+if ("isActive" == false) {
+  console.log("isActive");
+}
+
+let EULER = 2.718;
